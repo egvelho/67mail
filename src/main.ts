6d34606a0dab6c59525a55ae73ad704e031ec0a5
Eduarda Velho <email@eduardavelho.com>
2026-09-10 17:32:31 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import path from 'path';
+import * as hbs from 'hbs';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setBaseViewsDir(path.join(process.cwd(), 'src', 'views'));
+  app.engine('html', hbs.__express);
+  app.setViewEngine('html');
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      whitelist: true
-    })
+      whitelist: true,
+    }),
   );
 
   await app.listen(process.env.PORT ?? 3000);
